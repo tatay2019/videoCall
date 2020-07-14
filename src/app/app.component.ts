7eb7b0539b892644ee39201860b67b0083626597
tatay2019 <my.work.20199@gmail.com>
2020-07-14 17:18:21 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import Peer from 'peerjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'peerTopeer';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('myvideo', {static: true}) myVideo: any;
+  title = 'app works!';
+
+  targetpeer: any;
+  peer: any;
+  n = navigator as any;
+
+  ngOnInit() {
+    const video = this.myVideo.nativeElement;
+    let peerx: any;
+    this.n.getUserMedia = (this.n.getUserMedia || this.n.webkitGetUserMedia || this.n.mozGetUserMedia || this.n.msGetUserMedia);
+    // tslint:disable-next-line:only-arrow-functions
+    this.n.getUserMedia({video: true, audio: true}, function(stream) {
+      peerx = new SimplePeer ({
+        initiator: location.hash === '#room',
+        trickle: false,
+        stream
+      });
+
+      peerx.on('signal', function(data) {
+        console.log(JSON.stringify(data));
+        this.targetpeer = data;
+      });
+
+      // tslint:disable-next-line:only-arrow-functions
+      peerx.on('data', function(data) {
+        console.log('Recieved message:' + data);
+      });
+
+      // tslint:disable-next-line:only-arrow-functions
+      peerx.on('stream', function(re) {
+        const mediaStream = new MediaStream(re);
+        video.srcObject = mediaStream;
+        video.play();
+      });
+
+      // tslint:disable-next-line:only-arrow-functions
+    }, function(err){
+      console.log('Failed to get stream', err);
+    });
+
+    setTimeout(() => {
+      this.peer = peerx;
+      console.log(this.peer);
+    }, 5000);
+  }
+
+  connect() {
+    this.peer.signal(JSON.parse(this.targetpeer));
+  }
+
+  message() {
+    this.peer.send('Hello world');
+  }
+
+  ngAfterViewInit(): void {
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+///<reference path="typing.d.ts"/>
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
